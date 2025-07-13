1915664175161a8324d77cabb24aee118c4735bb
atefkhelifi <atef.khelifi@esprit.tn>
2025-07-13 14:32:06 +0200

    [fix:] fix port binding

--- app.py
+++ app.py
@@ -56,7 +56,8 @@ def detect():
     return jsonify({"specialist": specialist})
 
 if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port, debug=True)
 
